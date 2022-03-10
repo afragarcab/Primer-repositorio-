bb02f8c57e7a9f6ec176690fc52fec5de6a15f26
afragarcab <afragarcab@iestriana.com>
2022-03-10 13:05:53 +0100

    calculadora

--- JSCalculadora/js/funciones.js
+++ JSCalculadora/js/funciones.js
@@ -1,1 +1,39 @@
-//Para calcular hay que usar el eval
+//Calculadora del chino
+
+var resultado = "";
+
+function calcularSuma(){
+    var num1 = parseInt(document.getElementById("numero1").value);
+    var num2 = parseInt(document.getElementById("numero2").value);
+
+    resultado = num1 + num2;
+
+    document.getElementById("resultado").value = resultado;
+}
+
+function calcularResta(){
+    var num1 = parseInt(document.getElementById("numero1").value);
+    var num2 = parseInt(document.getElementById("numero2").value);
+
+    resultado = num1 - num2;
+
+    document.getElementById("resultado").value = resultado;
+}
+
+function calcularMultiplicar(){
+    var num1 = parseInt(document.getElementById("numero1").value);
+    var num2 = parseInt(document.getElementById("numero2").value);
+
+    resultado = num1 * num2;
+
+    document.getElementById("resultado").value = resultado;
+}
+
+function calcularDivision(){
+    var num1 = parseInt(document.getElementById("numero1").value);
+    var num2 = parseInt(document.getElementById("numero2").value);
+
+    resultado = num1 / num2;
+
+    document.getElementById("resultado").value = resultado;
+}
