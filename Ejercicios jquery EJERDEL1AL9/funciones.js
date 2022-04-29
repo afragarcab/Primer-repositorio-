2a850485bb9667867ab4ea311d0cf8483ab9dab5
afragarcab <afragarcab@iestriana.com>
2022-04-29 15:23:36 +0200

    Jquery, Exxamen y estudios

---
+++ Ejercicios jquery EJERDEL1AL9/funciones.js
@@ -0,0 +1,14 @@
+let x = $(document);
+x.ready(inicializarEventos);
+
+function inicializarEventos() {
+  let x = $("#parrafos p");
+  x.each(resaltarParrafos);
+}
+
+function resaltarParrafos() {
+  let x = $(this);
+  if (x.text().length < 100) {
+    x.css("background-color", "#ff0");
+  }
+}
